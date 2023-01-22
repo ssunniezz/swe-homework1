bb9434f1ff228ad35fe55baf9db946eb52443916
sunniez <ssunniez@gmail.com>
2023-01-22 17:30:48 +0700

    fix bad codes

--- Hw1/api.py
+++ Hw1/api.py
@@ -14,19 +14,21 @@ API for listing stocks on every vending machine
 
 @api_blueprint.route('/api/stocks', methods=['GET'])
 def stock_list():
-    lst = []
+    # rename to a more meaningful name
+    stock_list = []
 
     vendings = table.Vending.query.all()
     for vending in vendings:
-        d = vending.json()
+        # rename to a more meaningful name
+        vending_info = vending.json()
 
         stocks = sorted(table.Stock.query.filter_by(vending_id=vending.id).all(), key=lambda s: s.name)
         stock = {s.name: s.amount for s in stocks}
 
-        d['stock'] = stock
-        lst.append(d)
+        vending_info['stock'] = stock
+        stock_list.append(vending_info)
 
-    return jsonify(lst)
+    return jsonify(stock_list)
 
 
 '''
@@ -37,14 +39,15 @@ API for adding new vending machine, data were received through form-data
 @api_blueprint.route('/api/addVending', methods=['POST'])
 def add_vending():
     cols = table.Vending.__table__.columns
-    data = {c.name: request.form.get(c.name) for c in cols}
+    # rename to a more meaningful name
+    request_data = {c.name: request.form.get(c.name) for c in cols}
 
-    if not data['name']:
+    if not request_data['name']:
         return {'success': False}
 
     new_vending = table.Vending()
-    for d in data:
-        setattr(new_vending, d, data[d])
+    for d in request_data:
+        setattr(new_vending, d, request_data[d])
 
     db = app.db
     db.session.add(new_vending)
@@ -60,8 +63,9 @@ API for deleting vending machine by id, id were received through form-data
 
 @api_blueprint.route('/api/deleteVending', methods=['POST'])
 def delete_vending():
-    id = request.form.get('id')
-    to_delete = table.Vending.query.filter_by(id=id).first()
+    # rename to a more meaningful name
+    vending_id = request.form.get('id')
+    to_delete = table.Vending.query.filter_by(id=vending_id).first()
 
     if not to_delete:
         return {'success': False}
@@ -80,19 +84,21 @@ API for editing vending machine by id, id and data were received through form-da
 
 @api_blueprint.route('/api/editVending', methods=['POST'])
 def edit_vending():
-    id = request.form.get('id')
-    to_edit = table.Vending.query.filter_by(id=id).first()
+    # rename to a more meaningful name
+    vending_id = request.form.get('id')
+    to_edit = table.Vending.query.filter_by(id=vending_id).first()
 
     if not to_edit:
         return {'success': False}
 
+    # rename to a more meaningful name
     cols = table.Vending.__table__.columns
-    data = {c.name: request.form.get(c.name) for c in cols}
-    data.pop('id')
+    vending_data = {c.name: request.form.get(c.name) for c in cols}
+    vending_data.pop('id')
 
-    for d in data:
-        if data[d]:
-            setattr(to_edit, d, data[d])
+    for d in vending_data:
+        if vending_data[d]:
+            setattr(to_edit, d, vending_data[d])
 
     db = app.db
     db.session.commit()
@@ -111,7 +117,11 @@ def add_stock():
     name = request.form.get('name').lower()
     amount = request.form.get('amount')
 
-    product = table.Stock(vending_id=vending_id, name=name, amount=amount)
+    # add try-catch for invalid vending_id
+    try:
+        product = table.Stock(vending_id=vending_id, name=name, amount=amount)
+    except:
+        return {'success': False}
 
     db = app.db
     db.session.add(product)
@@ -155,7 +165,8 @@ def edit_stock():
 
     to_edit = table.Stock.query.filter_by(vending_id=vending_id, name=name).first()
 
-    if not to_edit:
+    # return false in case that amount is not parsed
+    if not to_edit or not amount:
         return {'success': False}
 
     to_edit.amount = amount
